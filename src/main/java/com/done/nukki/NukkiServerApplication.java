b0306e352a0dfc13d9c4202aba62da444d28b9d6
nasangyeop <gigic5220@gmail.com>
2025-02-01 17:22:49 +0900

    Initial commit

---
+++ src/main/java/com/done/nukki/NukkiServerApplication.java
@@ -0,0 +1,13 @@
+package com.done.nukki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NukkiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NukkiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/done/nukki/NukkiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.done.nukki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NukkiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
